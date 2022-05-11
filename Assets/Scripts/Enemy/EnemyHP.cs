b2f202e3a2d1a4f9ce71f340d49ee6d194409c73
andongni <x102039@gmail.com>
2022-05-11 13:54:37 +0800

    add Enemy hp system

--- Assets/Scripts/Enemy/EnemyHP.cs
+++ Assets/Scripts/Enemy/EnemyHP.cs
@@ -11,9 +11,12 @@ namespace SkillDemo.Enemy
         public Slider slider;
         public TextMeshProUGUI damageText;
 
+        private Animator anim;
+
         [SerializeField]
         private float hp = 100;
 
+        [SerializeField]
         private float hpShowAnimTime = 2;
         private float AnimTimer = 0;
         private float playerAttackDamage;
@@ -22,17 +25,12 @@ namespace SkillDemo.Enemy
         private void Awake()
         {
             damageText.gameObject.SetActive(false);
+            anim = GetComponent<Animator>();
         }
 
         private void Update()
         {
             HpNumToSliderUI();
-            
-            if(callSetAndPlayDamageAnim)
-            {
-                SetAndPlayDamageAnim(playerAttackDamage);
-                callSetAndPlayDamageAnim = false;
-            }
         }
 
         private void HpNumToSliderUI()
@@ -50,32 +48,28 @@ namespace SkillDemo.Enemy
             playerAttackDamage = damage;
             hp -= playerAttackDamage;
             callSetAndPlayDamageAnim = true;
-        }
 
-        private void SetAndPlayDamageAnim(float damage)
-        {
-            AnimTimer = 0;
-            damageText.gameObject.SetActive(true);
-            damageText.text = "-" + damage.ToString();
-            damageText.alpha = 255;
             StartCoroutine(DamageAnim(damage));
-
         }
+
+        // private void SetAndPlayDamageAnim(float damage)
+        // {
+        //     AnimTimer = 0;
+        //     damageText.gameObject.SetActive(true);
+        //     damageText.text = "-" + damage.ToString();
+        //     damageText.alpha = 255;
+        //     StartCoroutine(DamageAnim(damage));
+
+        // }
 
         private IEnumerator DamageAnim(float damage)
         {
-            if (AnimTimer >= hpShowAnimTime)
-            {
-                damageText.gameObject.SetActive(false);
-                print("OVER");
-                yield return null;
-            }
-            print(damageText.alpha);
-            damageText.alpha -= ( 0.1f / hpShowAnimTime);
-            //print(damageText.alpha);
+            damageText.text = "-" + damage.ToString();
+            damageText.gameObject.SetActive(true);
 
-            AnimTimer += 0.1f;
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(hpShowAnimTime);
+
+            damageText.gameObject.SetActive(false);
         }
     }
 }
